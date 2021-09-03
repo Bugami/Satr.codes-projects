0c9e86040eebc98629e9bc34c7ab89c95f0b4cb7
Bugami <86521023+Bugami@users.noreply.github.com>
2021-09-03 15:36:30 +0300

    Convert time from 12 and 24 format function
    the function receive a time in string format and based on it will convert

--- output.py
+++ output.py
@@ -1,28 +1,32 @@
-def postFix(expr):
-    vals = expr.split(' ')
-    for i in vals:
-        try:
-            int(i)
-        except ValueError:
-            val1 = vals[vals.index(i)-2]
-            val2 = vals[vals.index(i)-1]
-            if i == '+':
-                result = int(val1) + int(val2)
-                vals = vals[vals.index(i)+1:]
-                vals.insert(0,result)
-            elif i == '-':
-                result = int(val1) - int(val2)
-                vals = vals[vals.index(i)+1:]
-                vals.insert(0,result)
-            elif i == '*':
-                result = int(val1) * int(val2)
-                vals = vals[vals.index(i)+1:]
-                vals.insert(0,result)
-            elif i == '/':
-                result = int(val1) / int(val2)
-                vals = vals[vals.index(i)+1:]
-                vals.insert(0,result)
-    return vals[0]
-
-list1 = '4 1 - 2 *'
-print(postFix(list1))
+def convert_time(time):
+    """ Convert time from 12h and 24h format"""
+    if 'am' in time:
+        time = time[:5]
+        if time[:2] == '12':
+            return '0'+time[2:]
+        else:
+            return time
+    elif 'pm' in time:
+        time = time[:5]
+        if time[:2] == '12':
+            return time
+        elif time[:2] in '10 11':
+            t1 = int(time[:2])+12
+            time=str(t1)+time[2:]
+            return time
+        else:
+            t1 = int(time[0])+12
+            time=str(t1)+time[1:]
+            return time        
+    else:
+        if time[:2] in '13 14 15 16 17 18 19 20 21 22 23':
+            t1 = int(time[:2]) - 12
+            time = str(t1) + time[2:] + ' pm'
+            return time
+        elif time[:2] in '12':
+            return time + " pm"
+        elif time[0] in '0':
+            return '12' + time[1:] + ' am'
+        else:
+            return time + ' am'
+    pass
